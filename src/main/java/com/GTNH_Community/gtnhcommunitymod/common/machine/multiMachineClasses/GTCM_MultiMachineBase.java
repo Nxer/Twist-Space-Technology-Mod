11f9e5491631f706bc2a977a96650bfa3192c82a
Nxer <306803661@qq.com>
2023-11-08 16:59:34 +0800

    spotless

--- src/main/java/com/GTNH_Community/gtnhcommunitymod/common/machine/multiMachineClasses/GTCM_MultiMachineBase.java
+++ src/main/java/com/GTNH_Community/gtnhcommunitymod/common/machine/multiMachineClasses/GTCM_MultiMachineBase.java
@@ -1,120 +1,120 @@
 package com.GTNH_Community.gtnhcommunitymod.common.machine.multiMachineClasses;
 
-import com.GTNH_Community.gtnhcommunitymod.common.machine.GT_TileEntity_HolySeparator;
+import javax.annotation.Nonnull;
+
+import net.minecraft.item.ItemStack;
+
+import org.jetbrains.annotations.ApiStatus;
+
 import com.gtnewhorizon.structurelib.alignment.constructable.IConstructable;
 import com.gtnewhorizon.structurelib.alignment.constructable.ISurvivalConstructable;
-import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
+
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.logic.ProcessingLogic;
 import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_ExtendedPowerMultiBlockBase;
 import gregtech.api.recipe.check.CheckRecipeResult;
 import gregtech.api.recipe.check.CheckRecipeResultRegistry;
-import net.minecraft.item.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
-
-import javax.annotation.Nonnull;
 
 public abstract class GTCM_MultiMachineBase<T extends GTCM_MultiMachineBase<T>>
-	extends GT_MetaTileEntity_ExtendedPowerMultiBlockBase<T>
-	implements IConstructable, ISurvivalConstructable {
-	
-	// region Class Constructor
-	public GTCM_MultiMachineBase(int aID, String aName, String aNameRegional) {
-		super(aID, aName, aNameRegional);
-	}
-	
-	public GTCM_MultiMachineBase(String aName) {
-		super(aName);
-	}
-	
-	// endregion
-	
-	// region Processing Logic
-	
-	/**
-	 * Creates logic to run recipe check based on recipemap. This runs only once, on class instantiation.
-	 * <p>
-	 * If this machine doesn't use recipemap or does some complex things, override {@link #checkProcessing()}.
-	 */
-	@ApiStatus.OverrideOnly
-	protected ProcessingLogic createProcessingLogic() {
-		return new ProcessingLogic();
-	}
-	
-	/**
-	 * Checks recipe and setup machine if it's successful.
-	 * <p>
-	 * For generic machine working with recipemap, use {@link #createProcessingLogic()} to make use of shared codebase.
-	 */
-	@Nonnull
-	@Override
-	public CheckRecipeResult checkProcessing() {
-		// If no logic is found, try legacy checkRecipe
-		if (processingLogic == null) {
-			return checkRecipe(mInventory[1]) ? CheckRecipeResultRegistry.SUCCESSFUL
-				       : CheckRecipeResultRegistry.NO_RECIPE;
-		}
-		
-		setupProcessingLogic(processingLogic);
-		
-		CheckRecipeResult result = doCheckRecipe();
-		result = postCheckRecipe(result, processingLogic);
-		// inputs are consumed at this point
-		updateSlots();
-		if (!result.wasSuccessful()) return result;
-		
-		mEfficiency = (10000 - (getIdealStatus() - getRepairStatus()) * 1000);
-		mEfficiencyIncrease = 10000;
-		mMaxProgresstime = processingLogic.getDuration();
-		setEnergyUsage(processingLogic);
-		
-		mOutputItems = processingLogic.getOutputItems();
-		mOutputFluids = processingLogic.getOutputFluids();
-		
-		return result;
-	}
-	
-	// region Overrides
-	@Override
-	public boolean addToMachineList(IGregTechTileEntity aTileEntity, int aBaseCasingIndex) {
-		return super.addToMachineList(aTileEntity, aBaseCasingIndex)
-			       || addExoticEnergyInputToMachineList(aTileEntity, aBaseCasingIndex);
-	}
-	@Override
-	public boolean isCorrectMachinePart(ItemStack aStack) {
-		return true;
-	}
-	
-	@Override
-	public int getMaxEfficiency(ItemStack aStack) {
-		return 10000;
-	}
-	
-	@Override
-	public int getDamageToComponent(ItemStack aStack) {
-		return 0;
-	}
-	
-	@Override
-	public boolean explodesOnComponentBreak(ItemStack aStack) {
-		return false;
-	}
-	
-	@Override
-	public boolean supportsVoidProtection() {
-		return true;
-	}
-	
-	@Override
-	public boolean supportsInputSeparation() {
-		return true;
-	}
-	
-	@Override
-	public boolean supportsBatchMode() {
-		return true;
-	}
-	
-	
-	// endregion
+    extends GT_MetaTileEntity_ExtendedPowerMultiBlockBase<T> implements IConstructable, ISurvivalConstructable {
+
+    // region Class Constructor
+    public GTCM_MultiMachineBase(int aID, String aName, String aNameRegional) {
+        super(aID, aName, aNameRegional);
+    }
+
+    public GTCM_MultiMachineBase(String aName) {
+        super(aName);
+    }
+
+    // endregion
+
+    // region Processing Logic
+
+    /**
+     * Creates logic to run recipe check based on recipemap. This runs only once, on class instantiation.
+     * <p>
+     * If this machine doesn't use recipemap or does some complex things, override {@link #checkProcessing()}.
+     */
+    @ApiStatus.OverrideOnly
+    protected ProcessingLogic createProcessingLogic() {
+        return new ProcessingLogic();
+    }
+
+    /**
+     * Checks recipe and setup machine if it's successful.
+     * <p>
+     * For generic machine working with recipemap, use {@link #createProcessingLogic()} to make use of shared codebase.
+     */
+    @Nonnull
+    @Override
+    public CheckRecipeResult checkProcessing() {
+        // If no logic is found, try legacy checkRecipe
+        if (processingLogic == null) {
+            return checkRecipe(mInventory[1]) ? CheckRecipeResultRegistry.SUCCESSFUL
+                : CheckRecipeResultRegistry.NO_RECIPE;
+        }
+
+        setupProcessingLogic(processingLogic);
+
+        CheckRecipeResult result = doCheckRecipe();
+        result = postCheckRecipe(result, processingLogic);
+        // inputs are consumed at this point
+        updateSlots();
+        if (!result.wasSuccessful()) return result;
+
+        mEfficiency = (10000 - (getIdealStatus() - getRepairStatus()) * 1000);
+        mEfficiencyIncrease = 10000;
+        mMaxProgresstime = processingLogic.getDuration();
+        setEnergyUsage(processingLogic);
+
+        mOutputItems = processingLogic.getOutputItems();
+        mOutputFluids = processingLogic.getOutputFluids();
+
+        return result;
+    }
+
+    // region Overrides
+    @Override
+    public boolean addToMachineList(IGregTechTileEntity aTileEntity, int aBaseCasingIndex) {
+        return super.addToMachineList(aTileEntity, aBaseCasingIndex)
+            || addExoticEnergyInputToMachineList(aTileEntity, aBaseCasingIndex);
+    }
+
+    @Override
+    public boolean isCorrectMachinePart(ItemStack aStack) {
+        return true;
+    }
+
+    @Override
+    public int getMaxEfficiency(ItemStack aStack) {
+        return 10000;
+    }
+
+    @Override
+    public int getDamageToComponent(ItemStack aStack) {
+        return 0;
+    }
+
+    @Override
+    public boolean explodesOnComponentBreak(ItemStack aStack) {
+        return false;
+    }
+
+    @Override
+    public boolean supportsVoidProtection() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsInputSeparation() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsBatchMode() {
+        return true;
+    }
+
+    // endregion
 }
