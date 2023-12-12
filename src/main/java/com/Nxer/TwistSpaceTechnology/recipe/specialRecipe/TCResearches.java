6d972527072343cca067b95ee4b79cb479795094
Nxer <306803661@qq.com>
2023-12-12 22:06:42 +0800

    change ElvenWorkshop research to one pick unlock type

--- src/main/java/com/Nxer/TwistSpaceTechnology/recipe/specialRecipe/TCResearches.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/recipe/specialRecipe/TCResearches.java
@@ -25,7 +25,11 @@ public class TCResearches {
             -9,
             3,
             GTCMItemList.ElvenWorkshop.get(1, 0));
+
         ElvenWorkshopResearch.setParents("BH_GAIA_PYLON");
+
+        ElvenWorkshopResearch.setSecondary();
+
         ElvenWorkshopResearch.setPages(
             new ResearchPage("tc.research_text.BH_ELVEN_WORKSHOP.1"),
             new ResearchPage("tc.research_text.BH_ELVEN_WORKSHOP.2"),
@@ -43,6 +47,37 @@ public class TCResearches {
                         Materials.Steeleaf.getPlates(1), new ItemStack(ModItems.spawnerMover, 1),
                         ItemList.Field_Generator_EV.get(1), ItemList.Casing_IV.get(1), Materials.Steeleaf.getPlates(1),
                         new ItemStack(ModItems.spawnerMover, 1) })));
+
         ElvenWorkshopResearch.registerResearchItem();
+//
+//        new ResearchItem(
+//            "BH_ELVEN_WORKSHOP",
+//            "botanichorizons",
+//            new AspectList(),
+//            4,
+//            -9,
+//            3,
+//            GTCMItemList.ElvenWorkshop.get(1, 0))
+//            .setParents("BH_GAIA_PYLON")
+//            .setSecondary()
+//            .setPages(
+//                new ResearchPage("tc.research_text.BH_ELVEN_WORKSHOP.1"),
+//                new ResearchPage("tc.research_text.BH_ELVEN_WORKSHOP.2"),
+//                new ResearchPage(
+//                    new InfusionRecipe(
+//                        "BH_ELVEN_WORKSHOP",
+//                        GTCMItemList.ElvenWorkshop.get(1, 0),
+//                        10,
+//                        (new AspectList()).merge(Aspect.LIFE, 64)
+//                                          .merge(Aspect.EARTH, 64)
+//                                          .merge(Aspect.MAGIC, 64)
+//                                          .merge(Aspect.MECHANISM, 64),
+//                        new ItemStack(ModBlocks.terraPlate),
+//                        new ItemStack[] { ItemList.Field_Generator_EV.get(1), ItemList.Casing_IV.get(1),
+//                            Materials.Steeleaf.getPlates(1), new ItemStack(ModItems.spawnerMover, 1),
+//                            ItemList.Field_Generator_EV.get(1), ItemList.Casing_IV.get(1), Materials.Steeleaf.getPlates(1),
+//                            new ItemStack(ModItems.spawnerMover, 1) })))
+//            .registerResearchItem();
+
     }
 }
