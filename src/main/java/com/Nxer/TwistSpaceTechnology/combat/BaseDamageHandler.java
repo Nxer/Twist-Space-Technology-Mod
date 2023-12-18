9ee928e391ba7c65d7742cfc18d11d97b7c6eb01
The__Flames096 <superfty@126.com>
2023-12-18 21:25:03 +0800

    fix

--- src/main/java/com/Nxer/TwistSpaceTechnology/combat/BaseDamageHandler.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/combat/BaseDamageHandler.java
@@ -16,19 +16,19 @@ public class BaseDamageHandler {
         if (event.source.getEntity() instanceof EntityPlayer) {
             ArmorEventHandler.INSTANCE.updatePlayerStats((EntityPlayer) event.source.getEntity());
 
-            PlayerExtendedProperties SourceStats = PlayerExtendedProperties.instance
+            PlayerExtendedPropertSourceStats = PlayerExtendedProperties.instance
                 .from((EntityPlayer) event.source.getEntity());
             if ((event.source.getEntity().motionY < (double) 0) && !event.source.getEntity().onGround
                 && !event.source.getEntity()
                     .isRiding()
                 && event.source.damageType == "player")
                 damage = (float) (1.5
-                    * (1 + SourceStats.CombatStats.get("BaseDamage") + SourceStats.CombatStats.get("Strength") / 50.0));
+                    * ( SourceStats.CombatStats.get("BaseDamage") + SourceStats.CombatStats.get("Strength") / 50.0));
             else if (event.source.damageType != "indirectMagic")
                 damage = (float) (SourceStats.CombatStats.get("BaseDamage")
                     + SourceStats.CombatStats.get("Strength") / 50.0
-                    + 1);
-            else damage = 1 + SourceStats.CombatStats.get("BaseDamage");
+                    );
+            else damage = SourceStats.CombatStats.get("BaseDamage");
 
         }
         if (event.entityLiving instanceof EntityPlayer && event.source.damageType != "outOfWorld"
