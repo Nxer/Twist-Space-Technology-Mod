41cda338c6a7591d760dd4549d4f57b797c79685
The__Flames096 <superfty@126.com>
2023-12-22 22:12:10 +0800

    stats add

--- src/main/java/com/Nxer/TwistSpaceTechnology/combat/StatsDefination.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/combat/StatsDefination.java
@@ -3,6 +3,7 @@ package com.Nxer.TwistSpaceTechnology.combat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.HashMap;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -30,50 +31,75 @@ public class StatsDefination {
             "MeleeDamageMultipiler",
             "RangeDamageMultipiler",
             "MagicDamageMultipiler"));
-    // Str,Int,CC,CD,Res,BaseDamage,BaseDamageMultipiler,MeleeDamageMultipiler,RangeDamageMultipiler,MagicDamageMultipiler
-    public static final Map<String, int[]> ArmorStats = ImmutableMap.of(
-        "item.swordGold",
-        new int[] { 50, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        "item.swordDiamond",
-        new int[] { 100, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
 
+
+/*
+ * boss list
+ * LV: TF:Naga,Lich Tinker:Slime King
+ * MV: Vailla:Wither TF:Hydra
+ * HV: Vailla:Ender Dragon TF:Ghost
+ * EV: TF:Snow Queen
+ */
+
+
+    // BaseDamage,Def,Str,Int,CC,CD,Res,BaseDamageMultipiler,MeleeDamageMultipiler,RangeDamageMultipiler,MagicDamageMultipiler
+    public static final Map<String, int[]> ArmorStats = new HashMap<String,int[]>();
+    static
+    {
+    //Swords
+        //ULV
+        ArmorStats.put("item.swordWood",
+        new int[] { 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+        ArmorStats.put("item.swordGold",
+        new int[] { 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+        ArmorStats.put("item.swordStone",
+        new int[] { 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+        ArmorStats.put("item.swordIron",
+        new int[] { 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+        ArmorStats.put("item.swordDiamond",
+        new int[] { 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+        //LV
+        //MV
+        ArmorStats.put("item.skullfire_sword",
+        new int[] { 15, 0, 30, 0, 0, 0, 0, 0, 0, 0, 0 });
+        //HV
+        //EV
+        //IV
+        ArmorStats.put("item.elementiumSword",
+        new int[] { 33, 0, 100, 20, 50, 0, 0, 0, 0, 0, 0 });
+        //LuV
+        //ZPM
+//        ArmorStats.put();
+    }
     public static void printWeaponStats(ItemStack weapon, EntityPlayer pl) {
         String itemname = weapon.getUnlocalizedName();
-        if (weapon.getAttributeModifiers() != null) GT_Utility.sendChatToPlayer(
-            pl,
-            weapon.getAttributeModifiers()
-                .toString());
-        int BaseDamage = 0;// weapon.getAttributeModifiers().get("generic.attack_damage")
         if (!ArmorStats.containsKey(itemname)) {
-            GT_Utility.sendChatToPlayer(pl, "Damage: +" + BaseDamage);
+            GT_Utility.sendChatToPlayer(pl, "Damage: +0");
             return;
         }
         int[] itemstats = ArmorStats.get(itemname);
-        GT_Utility.sendChatToPlayer(pl, "Damage: +" + (itemstats[5] + BaseDamage));
-        if (itemstats[0] != 0) GT_Utility.sendChatToPlayer(pl, "Strength: +" + itemstats[0]);
-        if (itemstats[1] != 0) GT_Utility.sendChatToPlayer(pl, "Intelligence: +" + itemstats[1]);
-        if (itemstats[2] != 0) GT_Utility.sendChatToPlayer(pl, "Crit Chance: +" + itemstats[2]);
-        if (itemstats[3] != 0) GT_Utility.sendChatToPlayer(pl, "Crit Damage: +" + itemstats[3]);
-        if (itemstats[4] != 0) GT_Utility.sendChatToPlayer(pl, "Resistance: +" + itemstats[4]);
+        GT_Utility.sendChatToPlayer(pl, "Damage: +" + itemstats[0]);
+        if (itemstats[2] != 0) GT_Utility.sendChatToPlayer(pl, "Strength: +" + itemstats[2]);
+        if (itemstats[3] != 0) GT_Utility.sendChatToPlayer(pl, "Intelligence: +" + itemstats[3]);
+        if (itemstats[4] != 0) GT_Utility.sendChatToPlayer(pl, "Crit Chance: +" + itemstats[4]);
+        if (itemstats[5] != 0) GT_Utility.sendChatToPlayer(pl, "Crit Damage: +" + itemstats[5]);
+        if (itemstats[1] != 0) GT_Utility.sendChatToPlayer(pl, "Defence: +" + itemstats[1]);
+        if (itemstats[6] != 0) GT_Utility.sendChatToPlayer(pl, "Resistance: +" + itemstats[6]);
     }
 
     public static void printArmorStats(ItemStack armor, EntityPlayer pl) {
         String itemname = armor.getUnlocalizedName();
-        if (armor.getAttributeModifiers() != null) GT_Utility.sendChatToPlayer(
-            pl,
-            armor.getAttributeModifiers()
-                .toString());
-        int BaseDefence = 0;
-        GT_Utility.sendChatToPlayer(pl, "Defence: +" + BaseDefence);
         if (!ArmorStats.containsKey(itemname)) {
+            GT_Utility.sendChatToPlayer(pl, "Defence: +0");
             return;
         }
         int[] itemstats = ArmorStats.get(itemname);
-        if (itemstats[0] != 0) GT_Utility.sendChatToPlayer(pl, "Strength: +" + itemstats[0]);
-        if (itemstats[1] != 0) GT_Utility.sendChatToPlayer(pl, "Intelligence: +" + itemstats[1]);
-        if (itemstats[2] != 0) GT_Utility.sendChatToPlayer(pl, "Crit Chance: +" + itemstats[2]);
-        if (itemstats[3] != 0) GT_Utility.sendChatToPlayer(pl, "Crit Damage: +" + itemstats[3]);
-        if (itemstats[4] != 0) GT_Utility.sendChatToPlayer(pl, "Resistance: +" + itemstats[4]);
+        GT_Utility.sendChatToPlayer(pl, "Defence: +" + itemstats[1]);
+        if (itemstats[2] != 0) GT_Utility.sendChatToPlayer(pl, "Strength: +" + itemstats[2]);
+        if (itemstats[3] != 0) GT_Utility.sendChatToPlayer(pl, "Intelligence: +" + itemstats[3]);
+        if (itemstats[4] != 0) GT_Utility.sendChatToPlayer(pl, "Crit Chance: +" + itemstats[4]);
+        if (itemstats[5] != 0) GT_Utility.sendChatToPlayer(pl, "Crit Damage: +" + itemstats[5]);
+        if (itemstats[6] != 0) GT_Utility.sendChatToPlayer(pl, "Resistance: +" + itemstats[6]);
     }
 
 }
