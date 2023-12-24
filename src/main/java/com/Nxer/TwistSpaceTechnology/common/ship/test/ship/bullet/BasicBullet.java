1488cdc2f7cfc4002388240a8532af7535f9533f
liao <liao.yiqiang@qq.com>
2023-12-24 18:35:08 +0100

    add test

---
+++ src/main/java/com/Nxer/TwistSpaceTechnology/common/ship/test/ship/bullet/BasicBullet.java
@@ -0,0 +1,10 @@
+package com.Nxer.TwistSpaceTechnology.common.ship.test.ship.bullet;
+
+public class BasicBullet {
+
+    public int damage;
+
+    public BasicBullet(int damage) {
+        this.damage = damage;
+    }
+}
