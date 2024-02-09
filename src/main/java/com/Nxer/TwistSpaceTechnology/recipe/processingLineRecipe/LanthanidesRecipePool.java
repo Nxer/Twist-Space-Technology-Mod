29e78f2dbe7a0cccb5cbe7a28e081813da5aa68b
Nxer <306803661@qq.com>
2024-02-10 06:13:13 +0800

    add dust of Monazite and Bastnasite to Lanthanides processing

--- src/main/java/com/Nxer/TwistSpaceTechnology/recipe/processingLineRecipe/LanthanidesRecipePool.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/recipe/processingLineRecipe/LanthanidesRecipePool.java
@@ -1,5 +1,6 @@
 package com.Nxer.TwistSpaceTechnology.recipe.processingLineRecipe;
 
+import static com.Nxer.TwistSpaceTechnology.util.enums.TierEU.RECIPE_EV;
 import static com.Nxer.TwistSpaceTechnology.util.enums.TierEU.RECIPE_ZPM;
 
 import com.Nxer.TwistSpaceTechnology.recipe.IRecipePool;
@@ -26,6 +27,30 @@ public class LanthanidesRecipePool implements IRecipePool {
             .noOptimize()
             .eut(RECIPE_ZPM)
             .duration(40)
+            .addTo(digester);
+
+        // add dust of Monazite to Lanthanides process
+        GT_Values.RA.stdBuilder()
+            .itemInputs(Materials.Monazite.getDust(2))
+            .fluidInputs(Materials.NitricAcid.getFluid(700))
+            .itemOutputs(Materials.SiliconDioxide.getDust(1))
+            .fluidOutputs(WerkstoffMaterialPool.MuddyRareEarthMonaziteSolution.getFluidOrGas(400))
+            .specialValue(800)
+            .noOptimize()
+            .eut(RECIPE_EV)
+            .duration(20 * 20)
+            .addTo(digester);
+
+        // add dust of Bastnasite to Lanthanides process
+        GT_Values.RA.stdBuilder()
+            .itemInputs(Materials.Bastnasite.getDust(2))
+            .fluidInputs(Materials.NitricAcid.getFluid(700))
+            .itemOutputs(Materials.SiliconDioxide.getDust(1))
+            .fluidOutputs(WerkstoffMaterialPool.MuddyRareEarthBastnasiteSolution.getFluidOrGas(400))
+            .specialValue(800)
+            .noOptimize()
+            .eut(RECIPE_EV)
+            .duration(20 * 20)
             .addTo(digester);
 
     }
