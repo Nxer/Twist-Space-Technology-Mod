e03724b20e187726d389900d47c0f1f97b73791a
starxingchenc <1340364178@qq.com>
2024-02-16 17:48:58 +0800

    fix

--- src/main/java/com/Nxer/TwistSpaceTechnology/common/machine/singleBlock/hatch/GT_MetaTileEntity_Hatch_DualInput.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/common/machine/singleBlock/hatch/GT_MetaTileEntity_Hatch_DualInput.java
@@ -326,7 +326,7 @@ public class GT_MetaTileEntity_Hatch_DualInput extends GT_MetaTileEntity_Hatch_I
     public FluidStack drain(int maxDrain, boolean doDrain) {
         FulidStack nowFluid = getFluid();
         if (nowFluid == null) return null;
-        else return drain(ForgeDirection.UNKNOWN, new FluidStack(getFluid().getFluid(), maxDrain), doDrain);
+        else return drain(ForgeDirection.UNKNOWN, new FluidStack(nowFluid.getFluid(), maxDrain), doDrain);
     }
 
     @Override
