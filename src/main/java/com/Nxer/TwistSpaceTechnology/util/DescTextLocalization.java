8199ed404d22f5d8a9f53595e458342721bec520
初夏同学 <2411829240@qq.com>
2024-03-07 11:28:42 +0800

    Revert "Improve everything else"
    This reverts commit 9d6c74b6302918839ea92ca616baa5bd4871de3a.

--- src/main/java/com/Nxer/TwistSpaceTechnology/util/DescTextLocalization.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/util/DescTextLocalization.java
@@ -5,9 +5,7 @@ import net.minecraft.util.StatCollector;
 public class DescTextLocalization {
 
     // public static final String HeatCapacity = StatCollector.translateToLocal(TextLocalization.HeatCapacity);
-// #tr demo.key
-// #en_US Demo
-// #zh_CN 演示
+
     public static String[] addText(String preFix, int length) {
         String[] text = new String[length];
         for (int i = 0; i < length; i++) {
