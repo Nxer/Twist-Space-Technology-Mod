be5b59c4043f310f07a9b2fbca6099608be980f7
Goderium <Goderium@gmail.com>
2024-04-01 19:22:05 +0800

    fix Texture

--- src/main/java/com/Nxer/TwistSpaceTechnology/common/machine/TST_BallLightning.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/common/machine/TST_BallLightning.java
@@ -470,7 +470,7 @@ public class TST_BallLightning extends GTCM_MultiMachineBase<TST_BallLightning> 
                     GT_HatchElementBuilder.<TST_BallLightning>builder()
                         .atLeast(InputBus, OutputBus, InputHatch, OutputHatch, Energy.or(ExoticEnergy))
                         .adder(TST_BallLightning::addToMachineList)
-                        .casingIndex(((MetaBlockCasing01) BasicBlocks.MetaBlockCasing01).getTextureIndex(1))
+                        .casingIndex((BasicBlocks.MetaBlockCasing01.getTextureIndex(2)))
                         .dot(2)
                         .buildAndChain(MetaBlockCasing01, 1))
                 .addElement(
