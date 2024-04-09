1fcf1b7e13176cfb1ba8156578a322aed2590d11
xiaoxing2005 <xiaoxing2005@Outlook.com>
2024-04-09 22:57:36 +0800

    add MagesTower

--- src/main/java/com/Nxer/TwistSpaceTechnology/ClientProxy.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/ClientProxy.java
@@ -1,14 +1,18 @@
 package com.Nxer.TwistSpaceTechnology;
 
-import net.minecraftforge.common.MinecraftForge;
-
 import com.Nxer.TwistSpaceTechnology.client.render.ArtificialStarRender;
 import com.Nxer.TwistSpaceTechnology.common.machine.TST_BigBroArray;
 import com.Nxer.TwistSpaceTechnology.system.ItemCooldown.CooldownEventHandler;
-
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
+import net.minecraftforge.common.MinecraftForge;
 
 public class ClientProxy extends CommonProxy {
+
+    public static IModelCustom PowerChairModel = null;
 
     // Override CommonProxy methods here, if you want a different behaviour on the client (e.g. registering renders).
     // Don't forget to call the super methods as well.
@@ -18,5 +22,17 @@ public class ClientProxy extends CommonProxy {
         new ArtificialStarRender();
         MinecraftForge.EVENT_BUS.register(new CooldownEventHandler());// load cooldown HUD
         TST_BigBroArray.initializeDefaultTextures();
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event){
+        super.postInit(event);
+        PowerChairModel = AdvancedModelLoader.loadModel(new ResourceLocation("gtnhcommunitymod:model/PowerChair.obj"));
+        /*ClientProxy.registerItemRenderers();*/
+    }
+
+    public static void registerItemRenderers()
+    {
+        /*MinecraftForgeClient.registerItemRenderer(BasicItems.PowerChair,new ItemRenderer(PowerChairModel,new ResourceLocation("gtnhcommunitymod","model/Star.png")));*/
     }
 }
