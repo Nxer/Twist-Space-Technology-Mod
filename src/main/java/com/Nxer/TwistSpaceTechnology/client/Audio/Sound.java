550a81b97ebbf03bf891452c21710521a0486ebc
xiaoxing2005 <xiaoxing2005@Outlook.com>
2024-04-14 16:40:57 +0800

    fix PowerChair Bug && Pre-add Yamato

--- src/main/java/com/Nxer/TwistSpaceTechnology/client/Audio/Sound.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/client/Audio/Sound.java
@@ -3,7 +3,13 @@ package com.Nxer.TwistSpaceTechnology.client.Audio;
 import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.util.ResourceLocation;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
 public class Sound extends PositionedSoundRecord {
+
+    public static Sound sound;
 
     public Sound(ResourceLocation soundResource, float volume, float pitch, boolean repeat, float xPosition,
         float yPosition, float zPosition) {
