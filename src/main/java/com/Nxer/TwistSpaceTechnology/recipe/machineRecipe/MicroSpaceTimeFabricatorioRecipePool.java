79761b4a9b6d4d730b196a778ec008334df07a31
Nxer <306803661@qq.com>
2024-08-21 18:01:11 +0800

    add Advanced recipes of Raw Tesseract and Quantum Anomaly

---
+++ src/main/java/com/Nxer/TwistSpaceTechnology/recipe/machineRecipe/MicroSpaceTimeFabricatorioRecipePool.java
@@ -0,0 +1,65 @@
+package com.Nxer.TwistSpaceTechnology.recipe.machineRecipe;
+
+import static com.Nxer.TwistSpaceTechnology.util.Utils.setStackSize;
+import static com.Nxer.TwistSpaceTechnology.util.enums.TierEU.RECIPE_UIV;
+import static com.Nxer.TwistSpaceTechnology.util.enums.TierEU.RECIPE_UXV;
+import static gtPlusPlus.core.material.Particle.GRAVITON;
+import static gtPlusPlus.core.material.Particle.UNKNOWN;
+import static gtPlusPlus.xmod.gregtech.api.enums.GregtechItemList.Laser_Lens_Special;
+
+import com.Nxer.TwistSpaceTechnology.common.GTCMItemList;
+import com.Nxer.TwistSpaceTechnology.common.recipeMap.GTCMRecipe;
+import com.Nxer.TwistSpaceTechnology.recipe.IRecipePool;
+import com.Nxer.TwistSpaceTechnology.util.recipes.TST_RecipeBuilder;
+
+import gregtech.api.enums.ItemList;
+import gregtech.api.enums.Materials;
+import gregtech.api.enums.MaterialsUEVplus;
+import gregtech.api.enums.OrePrefixes;
+import gregtech.api.util.GT_OreDictUnificator;
+import gregtech.api.util.GT_Utility;
+import gtPlusPlus.core.material.ELEMENT;
+import gtPlusPlus.core.material.Particle;
+
+public class MicroSpaceTimeFabricatorioRecipePool implements IRecipePool {
+
+    @Override
+    public void loadRecipes() {
+
+        // region Tesseract adv recipe
+        TST_RecipeBuilder.builder()
+            .itemInputs(
+                GT_Utility.getIntegratedCircuit(24),
+                GT_OreDictUnificator.get(OrePrefixes.frameGt, MaterialsUEVplus.Eternity, 1),
+                GTCMItemList.SeedsSpaceTime.get(1),
+                Laser_Lens_Special.get(16),
+                ELEMENT.STANDALONE.CELESTIAL_TUNGSTEN.getFoil(64))
+            .fluidInputs(
+                MaterialsUEVplus.ExcitedDTSC.getFluid(1000),
+                ELEMENT.STANDALONE.ASTRAL_TITANIUM.getFluidStack(144 * 64))
+            .itemOutputs(setStackSize(ItemList.Tesseract.get(1), 512))
+            .fluidOutputs(MaterialsUEVplus.DimensionallyTranscendentResidue.getFluid(3000))
+            .eut(RECIPE_UXV)
+            .duration(20 * 60)
+            .addTo(GTCMRecipe.MicroSpaceTimeFabricatorioRecipes);
+
+        // endregion
+
+        // region Quantum Anomaly adv recipe
+        TST_RecipeBuilder.builder()
+            .itemInputs(
+                GT_Utility.getIntegratedCircuit(24),
+                setStackSize(Particle.getBaseParticle(UNKNOWN), 64),
+                setStackSize(Particle.getBaseParticle(GRAVITON), 64),
+                GTCMItemList.Antimatter.get(1))
+            .fluidInputs(Materials.Hydrogen.getPlasma(1000))
+            .itemOutputs(Laser_Lens_Special.get(64))
+            .fluidOutputs(MaterialsUEVplus.DimensionallyTranscendentResidue.getFluid(500))
+            .eut(RECIPE_UIV)
+            .duration(20 * 60)
+            .addTo(GTCMRecipe.MicroSpaceTimeFabricatorioRecipes);
+
+        // endregion
+
+    }
+}
