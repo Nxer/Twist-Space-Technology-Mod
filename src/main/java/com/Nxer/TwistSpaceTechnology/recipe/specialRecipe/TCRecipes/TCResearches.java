b7d3e142aa50cf7f9ddf5bfd7f84e8c49b047eeb
Goderium <Goderium@gmail.com>
2024-08-27 16:56:14 +0800

    sync

--- src/main/java/com/Nxer/TwistSpaceTechnology/recipe/specialRecipe/TCRecipes/TCResearches.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/recipe/specialRecipe/TCRecipes/TCResearches.java
@@ -121,6 +121,9 @@ public class TCResearches {
                 // Machine
                 new ResearchItem(
                     "ECO_SPHERE_SIMULATOR",
+                    // #tr tc.research_name.ECO_SPHERE_SIMULATOR
+                    // # {\GREEN}Eco-Sphere Simulator
+                    // #zh_CN {\GREEN}拟似生态圈
                     "BASICS",
                     (new AspectList()).merge(Aspect.TREE, 1)
                         .merge(Aspect.MECHANISM, 1)
@@ -131,11 +134,11 @@ public class TCResearches {
                     7,
                     -9,
                     10,
-                    // spotless:off
-                    GTCMItemList.MegaTreeFarm.get(1,0)).setPages(
+                    GTCMItemList.MegaTreeFarm.get(1, 0)).setPages(
+                        // spotless:off
                         // #tr tc.research_text.ECO_SPHERE_SIMULATOR.1
                         // # null
-                        // #zh_CN 拟似生态圈是一种将奥术与科技融合的装置,用于模拟自然生态.<BR>使用方法<BR>将机器放置在合适的地点,并连接到可调节功率的外部电源.装置会自动汲取环境的源质,保障树木与水生生物的自然生长.<BR>注意事项<BR>高电压下运行时,尽管效率降低,总产出会有所增加,使用者应合理调整电力输入.<BR>水生生物通常不会异常,如果出现非列表生物,请立即通报上级部门.
+                        // #zh_CN 一种将奥术与科技融合的装置,用于模拟自然生态.<BR>使用方法<BR>将机器放置在合适的地点,并连接到可调节功率的外部电源.装置会自动汲取环境的源质,保障树木与水生生物的自然生长.<BR>注意事项<BR>高电压下运行时,尽管效率降低,总产出会有所增加,使用者应合理调整电力输入.<BR>水生生物通常不会异常,如果出现非列表生物,请立即通报上级部门.
                         new ResearchPage(TextEnums.tr("tc.research_text.ECO_SPHERE_SIMULATOR.1")),
                         new ResearchPage(
                             new InfusionRecipe(
@@ -180,10 +183,9 @@ public class TCResearches {
                         new ResearchPage(TextEnums.tr("tc.research_text.ECO_SPHERE_SIMULATOR.2")),
                         // #tr tc.research_text.ECO_SPHERE_SIMULATOR.3
                         // # null
-                        // #zh_CN 有传言称,遇见这种变异生物的神秘使,将会从中获得一种无法言明的启示.<BR>这种启示不仅能深化对奥术的理解,甚至能够推动机器本身的进化,使其功能变得更为强大且不可思议.<BR>尽管这种现象极其罕见,但那些执着于奥术探索的神秘使们,似乎从中看到了某种更高的追求,激发了他们对未知的渴望.
+                        // #zh_CN <LINE>有传言称,遇见这种变异生物的神秘使,将会从中获得一种无法言明的启示.<BR>这种启示不仅能深化对奥术的理解,甚至能够推动机器本身的进化,使其功能变得更为强大且不可思议.<BR>尽管这种现象极其罕见,但那些执着于奥术探索的神秘使们,似乎从中看到了某种更高的追求,激发了他们对未知的渴望.
+                        // spotless:on
                         new ResearchPage(TextEnums.tr("tc.research_text.ECO_SPHERE_SIMULATOR.3")))
-                    // spotless:on
-
                         .setParents("INDUSTRIAL_MAGIC_MATRIX")
                         // .setHidden()
                         .setConcealed()
@@ -192,6 +194,9 @@ public class TCResearches {
                 // Offspring
                 new ResearchItem(
                     "OFFSPRING",
+                    // #tr tc.research_name.OFFSPRING
+                    // # {\DARK_AQUA}"Offspring"
+                    // #zh_CN {\DARK_AQUA}"子代"
                     "BASICS",
                     (new AspectList()).merge(Aspect.WATER, 1)
                         .merge(Aspect.EXCHANGE, 1)
@@ -203,8 +208,7 @@ public class TCResearches {
                         // #tr tc.research_text.OFFSPRING.1
                         // # null
                         // #zh_CN 这是什么?
-                        .setPages(
-                            new ResearchPage("tc.research_text.OFFSPRING.1"))
+                        .setPages(new ResearchPage("tc.research_text.OFFSPRING.1"))
                         .setParents("ECO_SPHERE_SIMULATOR")
                         .setConcealed()
                         .registerResearchItem();
@@ -212,6 +216,9 @@ public class TCResearches {
                 // Font of Ecology
                 new ResearchItem(
                     "FONT_OF_ECOLOGY",
+                    // #tr tc.research_name.FONT_OF_ECOLOGY
+                    // # {\BLUE}{\BOLD}Font of Ecology
+                    // #zh_CN {\BLUE}{\BOLD}生态泉源
                     "BASICS",
                     (new AspectList()).add(EVOLUTION, 1)
                         .add(Aspect.ENTROPY, 1)
@@ -224,35 +231,40 @@ public class TCResearches {
                     9,
                     -11,
                     10,
-                    GTCMItemList.FountOfEcology.get(1, 0))
-                        .setPages(
-                            // #tr tc.research_text.FONT_OF_ECOLOGY.1
-                            // # null
-                            // #zh_CN 这是什么?
-                            new ResearchPage("FONT_OF_ECOLOGY"),
-                            new ResearchPage(
-                                new InfusionRecipe(
-                                    "FONT_OF_ECOLOGY",
-                                    GTCMItemList.FountOfEcology.get(1),
-                                    200,
-                                    (new AspectList()).merge(EVOLUTION, 1024)
-                                        .merge(Aspect.WATER, 65536)
-                                        .merge(Aspect.LIFE, 16384)
-                                        .merge(Aspect.FLESH, 4096)
-                                        .merge(ULTRA_DEATH, 256),
-                                    Mods.Witchery.isModLoaded()
-                                        ? GT_ModHandler.getModItem(Mods.Witchery.ID, "infinityegg", 1)
-                                        : new ItemStack(Blocks.dragon_egg, 1),
-                                    new ItemStack[] { GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
-                                        GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
-                                        GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
-                                        GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
-                                        GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
-                                        GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
-                                        GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
-                                        GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
-                                        GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
-                                        GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1) })))
+                    GTCMItemList.FountOfEcology.get(1, 0)).setPages(
+                        // spotless:off
+                        // #tr tc.research_text.FONT_OF_ECOLOGY.1
+                        // # null
+                        // #zh_CN 在时间的漩涡中,生命的脉络编织成复杂的画卷,基因作为无形的笔触,暗中涂抹着自然的意志.这些沉默的串联悄然引导着生命的每一步演化.<BR>然而,在这看似秩序井然的背后,神秘使们发现了一只诡异的水母——一个隐匿于自然法则裂缝中的源头,凝聚了万物的基因表现,仿佛在低语着远古的秘密.<BR>它是所有生物进化的终极汇聚,包含着无数基因的交错与融合,仿佛宇宙对生命的反讽.  没有人知道它为什么在这里.
+                        new ResearchPage("tc.research_text.FONT_OF_ECOLOGY.1"),
+                        // #tr tc.research_text.FONT_OF_ECOLOGY.2
+                        // # null
+                        // #zh_CN 神秘使们试图解读这诡谲的现象,却发现它并非自然演化的产物,而是某种更为深邃的力量所引发的扭曲,仿佛自然本身在引导着生命朝向某个不可名状的终点.<BR>通过对它的逆向研究
+                        // spotless:on
+                        new ResearchPage("tc.research_text.FONT_OF_ECOLOGY.2"),
+                        new ResearchPage(
+                            new InfusionRecipe(
+                                "FONT_OF_ECOLOGY",
+                                GTCMItemList.FountOfEcology.get(1),
+                                200,
+                                (new AspectList()).merge(EVOLUTION, 1024)
+                                    .merge(Aspect.WATER, 65536)
+                                    .merge(Aspect.LIFE, 16384)
+                                    .merge(Aspect.FLESH, 4096)
+                                    .merge(ULTRA_DEATH, 256),
+                                Mods.Witchery.isModLoaded()
+                                    ? GT_ModHandler.getModItem(Mods.Witchery.ID, "infinityegg", 1)
+                                    : new ItemStack(Blocks.dragon_egg, 1),
+                                new ItemStack[] { GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
+                                    GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
+                                    GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
+                                    GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
+                                    GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
+                                    GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
+                                    GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
+                                    GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
+                                    GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1),
+                                    GTCMItemList.OffSpring.get(1), GTCMItemList.OffSpring.get(1) })))
                         .setParents("OFFSPRING")
                         .setConcealed()
                         .registerResearchItem();
