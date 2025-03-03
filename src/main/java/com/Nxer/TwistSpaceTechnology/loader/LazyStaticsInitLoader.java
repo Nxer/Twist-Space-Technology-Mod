617f646005ac35f74b8fc0d4e77022d17a0da8d1
Nxer <306803661@qq.com>
2025-03-03 13:58:24 +0800

    fix circuit converter issue

--- src/main/java/com/Nxer/TwistSpaceTechnology/loader/LazyStaticsInitLoader.java
+++ src/main/java/com/Nxer/TwistSpaceTechnology/loader/LazyStaticsInitLoader.java
@@ -4,7 +4,6 @@ import com.Nxer.TwistSpaceTechnology.common.machine.MiscHelper;
 import com.Nxer.TwistSpaceTechnology.common.machine.TST_MicroSpaceTimeFabricatorio;
 import com.Nxer.TwistSpaceTechnology.common.machine.TST_MiracleDoor;
 import com.Nxer.TwistSpaceTechnology.common.misc.CheckRecipeResults.CheckRecipeResults;
-import com.Nxer.TwistSpaceTechnology.system.CircuitConverter.machines.TST_CircuitConverter;
 import com.Nxer.TwistSpaceTechnology.system.DysonSphereProgram.machines.TST_ArtificialStar;
 import com.Nxer.TwistSpaceTechnology.system.DysonSphereProgram.machines.TST_DSPLauncher;
 import com.Nxer.TwistSpaceTechnology.system.DysonSphereProgram.machines.TST_DSPReceiver;
@@ -23,7 +22,6 @@ public class LazyStaticsInitLoader {
         TST_DSPReceiver.initStatics();
         TST_DSPLauncher.initStatics();
         TST_MicroSpaceTimeFabricatorio.initStatics();
-        TST_CircuitConverter.initStatics();
         TST_MiracleDoor.initStatics();
     }
 }
